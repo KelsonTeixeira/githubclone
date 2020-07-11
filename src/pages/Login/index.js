e0a5e5d368062f7cb49670ea8b0f75643ee3200d
KelsonTeixeira <txkelson@gmail.com>
2020-07-11 17:08:50 -0300

    change router Link to a submit button

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import Icon from './img/git.png';
 
 import './style.css';
@@ -14,18 +13,26 @@ export default class Login extends Component {
         console.log(this.state.inputValue);
     }
 
+    setSubmit = (event) => {
+        event.preventDefault();
+        this.props.history.push(`/userpage/${this.state.inputValue}`);
+    }
+
     render () {
         return (
             <div className='login-page'>
                 <div className='login-form' >
                     <img src={Icon} alt='github icon'/>
+
                     <h1>Sign in to GitHub copy</h1>
-                    <div className='username-box'>
-                        <label>GitHub's Username</label>
+
+                    <form className='username-box' onSubmit={this.setSubmit}>
+                        <label>GitHub Username:</label>
                         <input type='text' placeholder='username'
                          onChange={this.setValue} />
-                        <Link to={`/userpage/${this.state.inputValue}`}>Send</Link>
-                    </div>
+                        <button type="submit">Send</button>
+                    </form>
+
                     <a href='https://github.com'>This site github folder here</a>
                 </div>   
             </div>
